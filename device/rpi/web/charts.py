ee01cd2c3faa6655b004f68609c824985d75c199
dokeeffe <derek0207@gmail.com>
2019-03-01 20:51:05 +0000

    better cloud cover calc

--- device/rpi/web/charts.py
+++ device/rpi/web/charts.py
@@ -10,6 +10,7 @@ class ChartGenerator:
 
     NAME = 'ChartGenerator'
     HISTORICAL_DATA_SQL = "SELECT rain,sky_temperature, ambient_temperature, date_sensor_read FROM weather_sensor WHERE date_sensor_read >= date('now','-24 hour')"
+    MIN_SKY_TEMP_HISTORY =  "SELECT min(sky_temperature) as min_sky_temperature FROM weather_sensor WHERE date_sensor_read >= date('now','-10 day')"
 
     def __init__(self, root_dir):
         self.root_dir = root_dir
@@ -18,24 +19,26 @@ class ChartGenerator:
         self._conn = sqlite3.connect('weather_sensor.db')
 
 
-    def _calculate_cloud_cover(self, sky, ambient):
+    def _calculate_cloud_cover(self, sky, ambient, recent_clear_sky_temp):
         '''
-        Needs work. +22ambient and -2.5sky and sky was 90% clear
         :param sky:
         :param ambient:
+        :param recent_clear_sky_temp
         :return:
         '''
-        td = (33/100)*(ambient-0/10)+(4/100)*math.pow((math.exp(100/1000*ambient)), (100/100))
-        cc = sky-td
-        if cc > 10:
-            cc = 100
-        if cc < 1:
-            cc = 0
-        return cc
+        scaling_factor = 100 / abs(recent_clear_sky_temp)
+        x = abs(((sky * -1) * scaling_factor) - 100)
+        if x < 0:
+            return 0
+        elif x > 100:
+            return 100
+        else:
+            return x
 
     def generate_cloud_chart(self):
         weather = self._last_24hrs_data()
-        weather['CloudCover'] = weather.apply(lambda x: self._calculate_cloud_cover(x['sky_temperature'], x['ambient_temperature']), axis=1)
+        recent_clear_sky_temp = self._min_sky_last_month()
+        weather['CloudCover'] = weather.apply(lambda x: self._calculate_cloud_cover(x['sky_temperature'], x['ambient_temperature'], recent_clear_sky_temp), axis=1)
         weather['Rain'] = weather[['rain']]*100
         rain_clouds = weather.loc[:,['Rain','CloudCover']]
         rain_clouds.resample('5T').mean().plot();
@@ -52,9 +55,21 @@ class ChartGenerator:
             weather.index = pd.to_datetime(weather.Time)
             return weather
 
+    def _min_sky_last_month(self):
+        conn = sqlite3.connect('weather_sensor.db')
+        with conn:
+            cursor=conn.cursor()
+            cursor.execute(self.MIN_SKY_TEMP_HISTORY)
+            mintemp = cursor.fetchone()[0]
+            if mintemp > -5:
+                return -20 #-20 is a good guess for 100% clear sky
+            else:
+                return mintemp
+
     def generate_temperature_chart(self):
         weather = self._last_24hrs_data()
-        temperatures = weather.loc[:, ['sky_temperature','ambient_temperature']]
+        weather['diff'] = weather['ambient_temperature'] - weather['sky_temperature']
+        temperatures = weather.loc[:, ['sky_temperature','ambient_temperature', 'diff']]
         temperatures.plot();
         plt.savefig(os.path.join(self.root_dir, './temperature.png'), bbox_inches='tight')
         plt.clf()
